696af0e54bd28f15fe1b55da37871da9be1aac60
unknown <gsantaolalla2006@gmail.com>
2020-10-06 12:01:41 -0500

    Made some small changes to how the tags are displayed

--- src/commands/tags/getTag.js
+++ src/commands/tags/getTag.js
@@ -1,25 +1,31 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
+const commandUsage = require("../../utils/commandUsage.js");
 
 exports.run = async (client, message, args) => {
   const tag = args[0];
-  if (!tag) return;
+  if (!tag) return commandUsage.missingParams(message, "Tag to see", "getTag");
+  console.log(db.get("tag"));
   const data = await db.get(`tag.${message.guild.id}.${tag}`);
+  if (!data)
+    return message.channel.send(`There are no messages tagged with **${tag}**`);
 
   let messageNum = 1;
-  if (!data) return message.channel.send("NO data");
+
+  message.react("📨");
+
   data.forEach(async (taggedMessage) => {
     const author = message.guild.members.resolve(taggedMessage.author);
     const embed = new Discord.MessageEmbed()
       .setColor(0xff9f01)
       .setAuthor(
-        `Message by ${author.user.username}`,
+        `Message by ${author.user.tag || "User Not Found"}`,
         author.user.displayAvatarURL({ size: 2048 })
       )
       .setDescription(`Content:\n${taggedMessage.content}`)
       .setFooter(`Tag ${tag} Number ${messageNum}`);
-    await message.channel.send(embed);
-    messageNum += 1;
+    messageNum = messageNum + 1;
+    await message.author.send(embed);
   });
 };
 

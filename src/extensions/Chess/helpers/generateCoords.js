df8bb3e51b8b0e575105d732f0e7a632eae6556e
TechnoDrive <zarifabrar02@gmail.com>
2021-02-07 14:55:31 -0500

    Add chess functionality

---
+++ src/extensions/Chess/helpers/generateCoords.js
@@ -0,0 +1,14 @@
+let coords = [],
+    rows = 8,
+    cols = 8,
+    spacing = 50;
+
+for (let row = 0; row < rows; row++) {
+    let temp = [];
+    for (let col = 0; col < cols; col++) {
+        temp.push([col * spacing, row * spacing]);
+    }
+    coords.push(temp);
+}
+
+console.log(coords.flat())
--- src/index.js
+++ src/index.js
@@ -9,20 +9,20 @@ const extensions = require("./extensions");
 
 const devlaunchersBot = require("./handler/ClientBuilder.js");
 const client = new devlaunchersBot({
-  partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"],
+    partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"],
 });
 
 const promClient = require("prom-client");
 
-const metrics = registerMetrics();
-startMetricsServer(metrics);
+// const metrics = registerMetrics();
+// startMetricsServer(metrics);
 
 require("./handler/module.js")(client);
 require("./handler/Event.js")(client);
 
 client.package = require("../package.json");
 
-//require("dotenv").config();
+require("dotenv").config();
 
 client.on("warn", console.warn);
 client.on("error", console.error);
@@ -30,38 +30,38 @@ client.on("error", console.error);
 client.login(process.env.DISCORD_TOKEN);
 
 function registerMetrics() {
-  const collectDefaultMetrics = promClient.collectDefaultMetrics;
-  const Registry = promClient.Registry;
-  const register = new Registry();
-  collectDefaultMetrics({ register });
-  const events = new promClient.Counter({
-    name: "events_count",
-    help: "Count of discrod events",
-    labelNames: ["event"],
-  });
-  register.registerMetric(events);
-  return {
-    register: register,
-    events: events,
-  };
+    const collectDefaultMetrics = promClient.collectDefaultMetrics;
+    const Registry = promClient.Registry;
+    const register = new Registry();
+    collectDefaultMetrics({ register });
+    const events = new promClient.Counter({
+        name: "events_count",
+        help: "Count of discrod events",
+        labelNames: ["event"],
+    });
+    register.registerMetric(events);
+    return {
+        register: register,
+        events: events,
+    };
 }
 
 function startMetricsServer(metrics) {
-  const server = express();
-  server.get("/metrics", (req, res) => {
-    res.set("Content-Type", metrics.register.contentType);
-    res.end(metrics.register.metrics());
-  });
+    const server = express();
+    server.get("/metrics", (req, res) => {
+        res.set("Content-Type", metrics.register.contentType);
+        res.end(metrics.register.metrics());
+    });
 
-  const port = process.env.PORT || 3000;
-  console.log(
-    `Metrics server listening to ${port}, metrics exposed on /metrics endpoint`
-  );
-  server.listen(port);
+    const port = process.env.PORT || 3000;
+    console.log(
+        `Metrics server listening to ${port}, metrics exposed on /metrics endpoint`
+    );
+    server.listen(port);
 }
 
 function sendEvent(metric) {
-  metrics.events.inc({ event: metric });
+    // metrics.events.inc({ event: metric });
 }
 
 exports.sendEvent = sendEvent;

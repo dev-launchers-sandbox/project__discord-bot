01e4b1b8d802cd50fdd78ac6d414e76905a3090d
TechnoDrive <zarifabrar02@gmail.com>
2021-02-13 23:26:32 -0500

    Make the README helpful to new contributors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-const Discord = require("discord.js");
 const express = require("express");
 
 // Bring in polyfills (running old version of node)
 require("./utils/polyfills.js");
-
-// Run extensions after initializing client, before doing any other work
-const extensions = require("./extensions");
 
 const devlaunchersBot = require("./handler/ClientBuilder.js");
 const client = new devlaunchersBot({
@@ -14,11 +10,57 @@ const client = new devlaunchersBot({
 
 const promClient = require("prom-client");
 
-const metrics = registerMetrics();
-startMetricsServer(metrics);
+const speedyBuilds = process.argv.includes("speedyBuilds");
+
+if (speedyBuilds) { // Deploying the server takes too much time for something so simple
+    console.log("SpeedyBuilds has been enabled, please turn it off when you're done.");
+
+    function sendEvent(metric) {}
+    exports.sendEvent = sendEvent;
+} else {
+    const metrics = registerMetrics();
+    startMetricsServer(metrics);
+
+    function registerMetrics() {
+        const collectDefaultMetrics = promClient.collectDefaultMetrics;
+        const Registry = promClient.Registry;
+        const register = new Registry();
+        collectDefaultMetrics({ register });
+        const events = new promClient.Counter({
+            name: "events_count",
+            help: "Count of discrod events",
+            labelNames: ["event"],
+        });
+        register.registerMetric(events);
+        return {
+            register: register,
+            events: events,
+        };
+    }
+
+    function startMetricsServer(metrics) {
+        const server = express();
+        server.get("/metrics", (req, res) => {
+            res.set("Content-Type", metrics.register.contentType);
+            res.end(metrics.register.metrics());
+        });
+
+        const port = process.env.PORT || 3000;
+        console.log(
+            `Metrics server listening to ${port}, metrics exposed on /metrics endpoint`
+        );
+        server.listen(port);
+    }
+
+    function sendEvent(metric) {
+        metrics.events.inc({ event: metric });
+    }
+    exports.sendEvent = sendEvent;
+}
 
 require("./handler/module.js")(client);
 require("./handler/Event.js")(client);
+
 
 client.package = require("../package.json");
 
@@ -28,40 +70,3 @@ client.on("warn", console.warn);
 client.on("error", console.error);
 
 client.login(process.env.DISCORD_TOKEN);
-
-function registerMetrics() {
-    const collectDefaultMetrics = promClient.collectDefaultMetrics;
-    const Registry = promClient.Registry;
-    const register = new Registry();
-    collectDefaultMetrics({ register });
-    const events = new promClient.Counter({
-        name: "events_count",
-        help: "Count of discrod events",
-        labelNames: ["event"],
-    });
-    register.registerMetric(events);
-    return {
-        register: register,
-        events: events,
-    };
-}
-
-function startMetricsServer(metrics) {
-    const server = express();
-    server.get("/metrics", (req, res) => {
-        res.set("Content-Type", metrics.register.contentType);
-        res.end(metrics.register.metrics());
-    });
-
-    const port = process.env.PORT || 3000;
-    console.log(
-        `Metrics server listening to ${port}, metrics exposed on /metrics endpoint`
-    );
-    server.listen(port);
-}
-
-function sendEvent(metric) {
-    metrics.events.inc({ event: metric });
-}
-
-exports.sendEvent = sendEvent;

d90887f8cf6ecd291d4097f426fbc7af2f516f62
TechnoDrive <zarifabrar02@gmail.com>
2021-02-18 20:01:51 -0500

    refactor: GuildExtension.js: removed unused variable

--- src/extensions/.common/extends/GuildExtension.js
+++ src/extensions/.common/extends/GuildExtension.js
@@ -1,12 +1,11 @@
-const Discord = require("discord.js");
 const { Structures } = require("discord.js");
 
 Structures.extend("Guild", (Guild) => {
-	class GuildExtension extends Guild {
-		constructor(client, data) {
-			super(client, data);
-		}
-	}
+    class GuildExtension extends Guild {
+        constructor(client, data) {
+            super(client, data);
+        }
+    }
 
-	return GuildExtension;
+    return GuildExtension;
 });

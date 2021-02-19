466eb4219a1c9fa271b3e77584c41beb5a13981c
TechnoDrive <zarifabrar02@gmail.com>
2021-02-18 20:06:54 -0500

    refactor: guildMemberAdd.js: removed unused variables, switch embed sending style

--- src/controllers/events-old/guildMemberAdd.js
+++ src/controllers/events-old/guildMemberAdd.js
@@ -1,34 +1,36 @@
-const Discord = require("discord.js");
 const updateCounters = require("../../utils/updateCounters.js");
-const db = require("quick.db");
 const metrics = require("../../index.js");
+const db = require("quick.db");
 
-module.exports = async (client, member) => {
-	metrics.sendEvent("guild_member_add");
-	updateCounters.updateCounters(member, client);
-	sendWelcomeEmbed(client, member);
+module.exports = async(client, member) => {
+    metrics.sendEvent("guild_member_add");
+    updateCounters.updateCounters(member, client);
+    sendWelcomeEmbed(member);
 };
 
-function sendWelcomeEmbed(client, member) {
-	let welcomeChannelID = db.get(`welcome.${member.guild.id}`);
-	if (!welcomeChannelID) return;
-	const welcomeChannel = member.guild.channels.resolve(welcomeChannelID);
-	if (!welcomeChannel) return;
-	let icon = member.guild.iconURL({ size: 2048, dynamic: true });
-	let avatar = member.user.displayAvatarURL({ size: 2048 });
-	if (!welcomeChannel) return;
+function sendWelcomeEmbed(member) {
+    let welcomeChannelID = db.get(`welcome.${member.guild.id}`);
+    if (!welcomeChannelID) return;
 
-	const welcomeEmbed = new Discord.MessageEmbed()
-		.setAuthor(`${member.user.tag}`, avatar)
-		.setDescription(`Welcome to DevLaunchers **${member.user.username}**`)
-		.setFooter(` | ${member.id}`, icon)
-		.setColor(0xff9f01);
-	welcomeChannel.send(welcomeEmbed);
-	welcomeChannel.send(member.user.toString()).then((m) => m.delete());
+    const welcomeChannel = member.guild.channels.resolve(welcomeChannelID);
+    if (!welcomeChannel) return;
 
-	let controlCenterID = db.get(`control-center.${member.guild.id}`) || "null";
-	let controlChannel = member.guild.channels.resolve(controlCenterID);
-	if (!controlChannel) return;
+    let avatar = member.user.displayAvatarURL({ size: 2048 });
+    let icon = member.guild.iconURL({ size: 2048, dynamic: true });
 
-	controlChannel.send(member.user.toString()).then((m) => m.delete());
+    if (!welcomeChannel) return;
+
+    welcomeChannel.sendEmbed({
+        color: 0xff9f01,
+        author: { name: `${member.user.tag}`, image: avatar },
+        description: `Welcome to DevLaunchers **${member.user.username}**`,
+        footer: { text: ` | ${member.id}`, image: icon }
+    });
+    welcomeChannel.send(member.user.toString()).then((m) => m.delete());
+
+    let controlCenterID = db.get(`control-center.${member.guild.id}`) || "null";
+    let controlChannel = member.guild.channels.resolve(controlCenterID);
+    if (!controlChannel) return;
+
+    controlChannel.send(member.user.toString()).then((m) => m.delete());
 }

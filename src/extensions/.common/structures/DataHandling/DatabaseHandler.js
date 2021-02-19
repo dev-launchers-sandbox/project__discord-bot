04abd2541098b97e69ab366d537e8bd2c98f667e
TechnoDrive <zarifabrar02@gmail.com>
2021-02-18 20:11:46 -0500

    refactor: DatabaseHandler.js: make the BeanHandler actually work if used

--- src/extensions/.common/structures/DataHandling/DatabaseHandler.js
+++ src/extensions/.common/structures/DataHandling/DatabaseHandler.js
@@ -1,108 +1,107 @@
 const quickDB = require("quick.db");
 
 class DatabaseHandler {
-	constructor() {
-		this.bean = new BeanHandler();
-		this.reminder = new ReminderHandler();
-		this.invite = new InviteHandler();
-	}
+    constructor() {
+        this.bean = new BeanHandler();
+        this.reminder = new ReminderHandler();
+        this.invite = new InviteHandler();
+    }
 }
 
 class BeanHandler {
-	constructor() {}
+    constructor() {}
 
-	getUserDevBeans(userId) {
-		return quickDB.get(`account.${target.user.id}.devBeans`);
-	}
+    getUserDevBeans(id) {
+        return quickDB.get(`account.${id}.devBeans`);
+    }
 
-	getUserGoldenBeans(userId) {
-		return quickDB.get(`account.${target.user.id}.goldenBeans`);
-	}
+    getUserGoldenBeans(id) {
+        return quickDB.get(`account.${id}.goldenBeans`);
+    }
 
-	getUserForeverDevBeans(userId) {
-		return quickDB.get(`account.${target.user.id}.foreverDevBeans`);
-	}
+    getUserForeverDevBeans(id) {
+        return quickDB.get(`account.${id}.foreverDevBeans`);
+    }
 
-	getUserForeverGoldenBeans(userId) {
-		return quickDB.get(`account.${target.user.id}.foreverGoldenBeans`);
-	}
+    getUserForeverGoldenBeans(id) {
+        return quickDB.get(`account.${id}.foreverGoldenBeans`);
+    }
 }
 
 class ReminderHandler {
-	constructor() {}
+    constructor() {}
 
-	getReminders() {
-		return quickDB.get(`reminders`);
-	}
+    getReminders() {
+        return quickDB.get(`reminders`);
+    }
 
-	addReminder(reminder) {
-		quickDB.push(`reminders`, reminder);
-	}
+    addReminder(reminder) {
+        quickDB.push(`reminders`, reminder);
+    }
 
-	removeReminder(reminder) {
-		let reminders = this.getReminders();
-		let filtered = reminders.filter((entry) => {
-			return !(
-				entry.sentAt === reminder._sentAt &&
-				entry.userId === reminder._userId
-			);
-		});
-		this.setReminders(filtered);
-	}
+    removeReminder(reminder) {
+        let reminders = this.getReminders();
+        let filtered = reminders.filter((entry) => {
+            return !(
+                entry.sentAt === reminder._sentAt && entry.userId === reminder._userId
+            );
+        });
+        this.setReminders(filtered);
+    }
 
-	setReminders(reminders) {
-		quickDB.set(`reminders`, reminders);
-		return reminders;
-	}
+    setReminders(reminders) {
+        quickDB.set(`reminders`, reminders);
+        return reminders;
+    }
 
-	getUserReminders(user) {
-		return quickDB.get(`reminders`).filter((entry) => {
-			return entry.userId == user.id;
-		});
-	}
+    getUserReminders(user) {
+        return quickDB.get(`reminders`).filter((entry) => {
+            return entry.userId == user.id;
+        });
+    }
 
-	popUserReminder(user) {
-		// Using global stack to emulate individual user stacks
-		let reminders = this.getReminders();
-		let reminder;
-		for (let i = reminders.length - 1; i >= 0; i--) {
-			if (user.id == reminders[i].userId) {
-				console.log("FOUND REMINDER");
-				reminder = reminders.splice(i, 1)[0];
-				break;
-			}
-		}
-		this.setReminders(reminders);
-		return reminder;
-	}
+    popUserReminder(user) {
+        // Using global stack to emulate individual user stacks
+        let reminders = this.getReminders();
+        let reminder;
+        for (let i = reminders.length - 1; i >= 0; i--) {
+            if (user.id == reminders[i].userId) {
+                console.log("FOUND REMINDER");
+                reminder = reminders.splice(i, 1)[0];
+                break;
+            }
+        }
+        this.setReminders(reminders);
+        return reminder;
+    }
 
-	clearReminders() {
-		quickDB.set(`reminders`, []);
-	}
+    clearReminders() {
+        quickDB.set(`reminders`, []);
+    }
 }
 
 class InviteHandler {
-	constructor() {}
+    constructor() {}
 
-	setInvite(guildId, name, code) {
-		quickDB.set(`invites.${guildId}.${name}`, code);
-	}
+    setInvite(guildId, name, code) {
+        quickDB.set(`invites.${guildId}.${name}`, code);
+    }
 
-	getInvites(guildId) {
-		return quickDB.get(`invites.${guildId}`);
-	}
+    getInvites(guildId) {
+        return quickDB.get(`invites.${guildId}`);
+    }
 
-	getInvite(guildId, name) {
-		return quickDB.get(`invites.${guildId}.${name}`);
-	}
+    getInvite(guildId, name) {
+        return quickDB.get(`invites.${guildId}.${name}`);
+    }
 
-	removeInvite(guildId, name) {
-		quickDB.delete(`invites.${guildId}.${name}`);
-	}
+    removeInvite(guildId, name) {
+        quickDB.delete(`invites.${guildId}.${name}`);
+    }
 
-	getInviteChannel(guildId) {
-		return quickDB.get(`invite.${guildId}`);
-	}
+    getInviteChannel(guildId) {
+        return quickDB.get(`invite.${guildId}`);
+    }
 }
 
 module.exports = new DatabaseHandler();

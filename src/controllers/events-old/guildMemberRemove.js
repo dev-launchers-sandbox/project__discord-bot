2ca569b41075dc149584ec2fda7fa383b1333ce2
TechnoDrive <zarifabrar02@gmail.com>
2021-02-18 20:08:38 -0500

    refactor: guildMemberRemove.js: removed unused variable

--- src/controllers/events-old/guildMemberRemove.js
+++ src/controllers/events-old/guildMemberRemove.js
@@ -1,8 +1,7 @@
-const Discord = require("discord.js");
 const updateCounters = require("../../utils/updateCounters.js");
 const metrics = require("../../index.js");
 
-module.exports = async (client, member) => {
-	metrics.sendEvent("guild_member_remove");
-	updateCounters.updateCounters(member, client);
+module.exports = async(client, member) => {
+    metrics.sendEvent("guild_member_remove");
+    updateCounters.updateCounters(member, client);
 };

557f7fa28b129b9bb2fafbfc2917544fd8b4ca65
Guillermo S <gsantaolalla2006@gmail.com>
2021-03-01 14:04:28 -0600

    Added thread custom invites.

--- src/extensions/Thread/controllers/events/threadOnReactionAdd.js
+++ src/extensions/Thread/controllers/events/threadOnReactionAdd.js
@@ -6,28 +6,45 @@ exports.eventHandle = "messageReactionAdd";
 exports.event = async (client, messageReaction, user) => {
   metrics.sendEvent("message_reaction_add");
   if (user.bot) return;
-  if (messageReaction.emoji.name === "🧵") {
-    let message = messageReaction.message;
-    let threads = dbh.thread.getThreads();
+  let message = messageReaction.message;
+  let thread = getThread(messageReaction);
 
-    const thread = threads.find((t) => t.invites.includes(message.id));
+  if (!thread) return;
+  let guild = message.guild;
+  if (!guild) return;
+  let threadChannel = guild.channels.resolve(thread.channelId);
+  if (!threadChannel) return;
 
-    if (!thread) return;
-    let guild = message.guild;
-    if (!guild) return;
-    let threadChannel = guild.channels.resolve(thread.channelId);
-    if (!threadChannel) return;
+  if (thread.blacklist.includes(user.id)) {
+    user.send(`You are blacklisted from ${threadChannel.name}!`);
+    return;
+  }
 
-    if (thread.blacklist.includes(user.id)) {
-      user.send(`You are blacklisted from ${threadChannel.name}!`);
-      return;
-    }
+  let member = guild.members.resolve(user.id);
 
-    let member = guild.members.resolve(user.id);
-
-    if (!member.roles.cache.find((r) => r.id === thread.roleId)) {
-      member.roles.add(thread.roleId);
-      threadChannel.send(`${member.toString()} joined the thread.`);
-    }
+  if (!member.roles.cache.find((r) => r.id === thread.roleId)) {
+    member.roles.add(thread.roleId);
+    threadChannel.send(`${member.toString()} joined the thread.`);
   }
 };
+
+function getThread(messageReaction) {
+  let threads = dbh.thread.getThreads();
+  let message = messageReaction.message;
+  let thread;
+
+  if (messageReaction.emoji.name === "🧵") {
+    thread = threads.find((t) => t.invites.includes(message.id));
+  } else {
+    thread = threads.find((t) => {
+      if (!t.customInvites) return false;
+      return t.customInvites.find(
+        (i) =>
+          i.id === message.id &&
+          i.reaction ===
+            `<:${messageReaction.emoji.name}:${messageReaction.emoji.id}>`
+      );
+    });
+  }
+  return thread;
+}
--- src/extensions/Thread/controllers/events/threadOnReactionRemove.js
+++ src/extensions/Thread/controllers/events/threadOnReactionRemove.js
@@ -6,23 +6,40 @@ exports.eventHandle = "messageReactionRemove";
 exports.event = async (client, messageReaction, user) => {
   metrics.sendEvent("message_reaction_remove");
   if (user.bot) return;
-  if (messageReaction.emoji.name === "🧵") {
-    let message = messageReaction.message;
-    let threads = dbh.thread.getThreads();
+  let message = messageReaction.message;
+  let thread = getThread(messageReaction);
 
-    const thread = threads.find((t) => t.invites.includes(message.id));
+  if (!thread) return;
+  let guild = message.guild;
+  if (!guild) return;
+  let threadChannel = guild.channels.resolve(thread.channelId);
+  if (!threadChannel) return;
 
-    if (!thread) return;
-    let guild = message.guild;
-    if (!guild) return;
-    let threadChannel = guild.channels.resolve(thread.channelId);
-    if (!threadChannel) return;
+  let member = guild.members.resolve(user.id);
 
-    let member = guild.members.resolve(user.id);
-
-    if (member.roles.cache.find((r) => r.id === thread.roleId)) {
-      member.roles.remove(thread.roleId);
-      threadChannel.send(`${member.toString()} has left the thread.`);
-    }
+  if (member.roles.cache.find((r) => r.id === thread.roleId)) {
+    member.roles.remove(thread.roleId);
+    threadChannel.send(`${member.toString()} left the thread.`);
   }
 };
+
+function getThread(messageReaction) {
+  let threads = dbh.thread.getThreads();
+  let message = messageReaction.message;
+  let thread;
+
+  if (messageReaction.emoji.name === "🧵") {
+    thread = threads.find((t) => t.invites.includes(message.id));
+  } else {
+    thread = threads.find((t) => {
+      if (!t.customInvites) return false;
+      return t.customInvites.find(
+        (i) =>
+          i.id === message.id &&
+          i.reaction ===
+            `<:${messageReaction.emoji.name}:${messageReaction.emoji.id}>`
+      );
+    });
+  }
+  return thread;
+}

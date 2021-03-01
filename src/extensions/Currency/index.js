7433d3f333e3f84baee9336e5a87c99264abca4e
Guillermo S <gsantaolalla2006@gmail.com>
2021-03-01 12:01:36 -0600

    Added the skeleton for the Currency Plugin.

---
+++ src/extensions/Currency/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "Currency",
+  helpCategory: "Currency",
+  helpPage: 3,
+  commands: [],
+  events: [],
+  extends: [],
+  structures: [require("./structures/CurrencyManager.js")],
+  permissions: [],
+};
---
+++ src/extensions/Currency/structures/CurrencyManager.js
@@ -0,0 +1,5 @@
+class CurrencyManager {
+  constructor() {}
+}
+
+module.exports = new CurrencyManager();

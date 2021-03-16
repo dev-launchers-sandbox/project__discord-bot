095427396d91d463885398ebb73a6bf239c5c065
Kris Gano <kris@pyxld.com>
2021-03-15 22:46:24 -0500

    Impement basic cumulative sum algo for Box Drops

--- src/plugins/NitroEngine/structures/PartBox.js
+++ src/plugins/NitroEngine/structures/PartBox.js
@@ -2,19 +2,35 @@
 
 const NitroEnginePart = require("./NitroEnginePart.js");
 
-const partTypes = [
-  "Combustion Chamber",
-  "Carburetor",
-  "Piston",
-  "Cylinder",
-  "Exhaust",
+const DROP_TABLE = [
+  { type: "Fuel", dropWeight: 100 },
+  { type: "Combustion Chamber", dropWeight: 20 },
+  { type: "Carburetor", dropWeight: 20 },
+  { type: "Piston", dropWeight: 20 },
+  { type: "Cylinder", dropWeight: 20 },
+  { type: "Exhaust", dropWeight: 20 },
 ];
 
 class PartBox {
   constructor() {}
 
   getRandomPartType() {
-    return partTypes[parseInt(Math.random() * partTypes.length)];
+    let cumulativeSum = 0;
+    let cumulativeValues = [];
+    DROP_TABLE.forEach((entry) => {
+      cumulativeSum += entry.dropWeight;
+      cumulativeValues.push(cumulativeSum);
+    });
+    let randNum = parseInt(Math.random() * cumulativeSum);
+    let index = -1;
+    cumulativeValues.forEach((sumVal, i) => {
+      if (randNum < sumVal) {
+        index = i;
+        break;
+      }
+    });
+
+    return partTypes[index];
   }
 
   open() {

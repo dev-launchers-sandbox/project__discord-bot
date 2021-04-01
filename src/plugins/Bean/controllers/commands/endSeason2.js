14e55a2109846f2e57316760870a68c990d1be7a
Guillermo S <gsantaolalla2006@gmail.com>
2021-04-01 16:59:47 -0500

    Added the command endSeason2.

--- src/plugins/Bean/controllers/commands/endSeason2.js
+++ src/plugins/Bean/controllers/commands/endSeason2.js
@@ -18,11 +18,14 @@ exports.run = async (client, message, args) => {
   if (!client.config.owners.includes(message.author.id)) return;
 
   let members = await message.guild.members.fetch();
-
+  let userNum = 1;
   try {
     await members.forEach(async (member) => {
       try {
+        console.log(`attempting to delete user #${userNum}`);
+        await db.delete(`account.${member.id}.devBeans`);
         await db.delete(`account.${member.id}.goldenBeans`);
+        userNum++;
       } catch (e) {
         console.log(e);
       }

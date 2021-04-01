0ece081b811527324b056cf308a91decb8e68ff4
Guillermo S <gsantaolalla2006@gmail.com>
2021-04-01 17:49:49 -0500

    Added a command to remove the beans from one specific user.

---
+++ src/plugins/UtilCommands/controllers/commands/removeBeans.js
@@ -0,0 +1,19 @@
+exports.help = {
+  name: "removeBeans",
+  description: "Removes the beans from a player",
+  usage: "removeBeans <id>",
+  example: "removeBeans 63624686420492340",
+};
+
+exports.conf = {
+  aliases: ["reacto", "react"],
+  cooldown: 5,
+  permissions: ["ADMINISTRATOR"],
+  arguments: ["User"],
+};
+
+exports.run = async (client, message, args) => {
+  const id = args[0] || "-";
+  db.delete(`account.${id}.devBeans`);
+  db.delete(`account.${id}.goldenBeans`);
+};

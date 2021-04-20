189d9a92df1bf08ef5fa1bf9eb2a5815796a6394
Guillermo S <gsantaolalla2006@gmail.com>
2021-04-19 21:43:31 -0500

    Remove old code

--- src/controllers/events-old/messageReactionAdd.js
+++ src/controllers/events-old/messageReactionAdd.js
@@ -36,14 +36,6 @@ async function removeEmoji(messageReaction, user) {
 
 module.exports = async (client, messageReaction, user) => {
   metrics.sendEvent("message_reaction_add");
-  if (messageReaction.emoji.name === "DevBean") {
-    let message = await fetchMessage(client, messageReaction, user);
-    return awardDevBean(client, message, user);
-  }
-  if (messageReaction.emoji.name === "GoldenBean") {
-    let message = await fetchMessage(client, messageReaction, user);
-    return awardGoldenBean(client, message, user);
-  }
   if (messageReaction.emoji.name === "🎟️") {
     let message = await fetchMessage(client, messageReaction, user);
     return openTicket(client, message, user);
@@ -53,91 +45,6 @@ module.exports = async (client, messageReaction, user) => {
     return giveMinecraftRole(client, message, user, messageReaction);
   }
 };
-
-async function awardDevBean(client, messageReaction, user) {
-  let userToGiveBeansTo = messageReaction.message.author.id; //id of the users whos message got a reaction
-  let userWhoReacted = user.id; //user that reacted
-  //if there were any bots involved in the message we do not want to continue
-  let cooldown = 60000;
-  let pad_zero = (num) => (num < 10 ? "0" : "") + num; //do not ask me what this does
-  let timeObj;
-  let lastDevBean = db.get(`lastDevBean.${user.id}`);
-  if (user.bot || messageReaction.message.author.bot) return;
-  //if the user reaction to his own message he will not get a devBean
-  if (userToGiveBeansTo === userWhoReacted) return;
-  try {
-    if (lastDevBean !== null && cooldown - (Date.now() - lastDevBean) > 0) {
-      timeObj = ms(cooldown - (Date.now() - lastDevBean));
-      let seconds = pad_zero(timeObj.seconds).padStart(2, "");
-      let finalTime = `**${seconds} second(s)**`;
-      await removeEmoji(messageReaction, user);
-      return user.send(`Please wait ${finalTime} before giving another Dev Bean!`);
-    } else {
-      db.set(`lastDevBean.${user.id}`, Date.now());
-      db.add(`account.${userToGiveBeansTo}.devBeans`, 1);
-      db.add(`account.${userToGiveBeansTo}.foreverDevBeans`, 1);
-      /*
-      user.send(
-        `Dev Bean added to **${messageReaction.message.author.tag}** balance!`
-      );
-      */
-      beanMessenger.sendDevBeanNotification(
-        user,
-        messageReaction.message.author,
-        messageReaction.message
-      );
-    }
-  } catch (err) {
-    console.log(err);
-    user.send("Oopsie, for some reason I could not award the user with the dev-bean");
-  }
-}
-
-async function awardGoldenBean(client, messageReaction, user) {
-  //if there were any bots involved in the message we do not want to continue
-  if (user.bot || messageReaction.message.author.bot) return;
-  //if the user does not react with the correct emoji, we do not want to do anything
-  if (messageReaction.emoji.name !== "GoldenBean") return;
-  //if the user is not an admininstrator, we do not want to award the user with a golden bean
-  let userToGiveGoldenBeansTo = messageReaction.message.author.id; //id of the users whos message got a reaction
-  let userWhoReacted = user.id; //user that reacted
-  let cooldown = 8.64e7;
-  let pad_zero = (num) => (num < 10 ? "0" : "") + num; //do not ask me what this does
-  //if the user reaction to his own message he will not get a devBean
-  if (userToGiveGoldenBeansTo === userWhoReacted) return;
-  let lastGoldenBean = db.get(`lastGoldenBean.${user.id}`);
-  let timeObj;
-  try {
-    if (lastGoldenBean !== null && cooldown - (Date.now() - lastGoldenBean) > 0) {
-      timeObj = ms(cooldown - (Date.now() - lastGoldenBean));
-      let hours = pad_zero(timeObj.hours).padStart(2, "0"),
-        minutes = pad_zero(timeObj.minutes).padStart(2, "");
-      let finalTime = `**${hours} hour(s) and ${minutes} minute(s)**`;
-      await removeEmoji(messageReaction, user);
-      return user.send(`Please wait ${finalTime} before giving another Golden Bean!`);
-    } else {
-      db.set(`lastGoldenBean.${user.id}`, Date.now());
-      db.add(`account.${userToGiveGoldenBeansTo}.goldenBeans`, 1);
-      db.add(`account.${userToGiveGoldenBeansTo}.foreverGoldenBeans`, 1);
-      /*
-      user.send(
-        `Golden Bean added to **${messageReaction.message.author.tag}** balance!`
-      );
-      */
-      beanMessenger.sendGoldenBeanNotification(
-        user,
-        messageReaction.message.author,
-        messageReaction.message
-      );
-
-      return db.set(`lastGoldenBeanAwarded.${user.id}`, messageReaction.message.id);
-    }
-  } catch (err) {
-    //if there is an error, send an "error" message
-    user.send("Oopsie, for some reason I could not award the user with the golden-bean");
-    console.log(err);
-  }
-}
 
 async function openTicket(client, messageReaction, user) {
   if (user.bot) return;

324650136ebafe60569c1735809ecfd1ed22d42c
gsantaolalla <gsantaolalla2006@gmail.com>
2021-07-04 13:43:20 -0500

    fix(guillermosanta): Create "endSeason" command

--- src/plugins/Bean/index.js
+++ src/plugins/Bean/index.js
@@ -1,13 +1,14 @@
 module.exports = {
-	name: "Bean",
-	helpCategory: "Beans",
-	helpPage: 3,
-	commands: [
-		require("./controllers/commands/beans.js"),
-		require("./controllers/commands/leaderboard.js"),
-	],
-	events: [require("./controllers/events/beanOnReactionAdd.js")],
-	extends: [require("./extends/BeanMessageReaction.js")],
-	structures: [],
-	permissions: [],
+  name: "Bean",
+  helpCategory: "Beans",
+  helpPage: 3,
+  commands: [
+    require("./controllers/commands/beans.js"),
+    require("./controllers/commands/endSeason.js"),
+    require("./controllers/commands/leaderboard.js"),
+  ],
+  events: [require("./controllers/events/beanOnReactionAdd.js")],
+  extends: [require("./extends/BeanMessageReaction.js")],
+  structures: [],
+  permissions: [],
 };
